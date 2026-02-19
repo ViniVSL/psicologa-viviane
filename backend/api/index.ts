f54687c3713a08d28a95a820604116ac01468159
ViniVSL <vinicius.santana.lima16@gmail.com>
2026-02-19 00:14:54 -0300

    fix all

--- backend/api/index.ts
+++ backend/api/index.ts
@@ -1,4 +1,8 @@
 import app from "../src/server.js";
 import serverless from "serverless-http";
 
-export default serverless(app);
+const handler = serverless(app);
+
+export default async (req: any, res: any) => {
+  return await handler(req, res);
+};
